fb6b901e213e2391bd46f964e408d64f88eb9a7f
arifvtti <arif.vtti@gmail.com>
2023-01-28 11:38:03 +0600

    material app, scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 
-void main() {
+main() {
   runApp(MyApp());
 }
 
@@ -12,60 +12,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Paper Less"),
-          actions: [Icon(Icons.share)],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                  padding: EdgeInsets.all(0),
-                  child: UserAccountsDrawerHeader(
-                    currentAccountPicture: Center(
-                      child: Image.network(
-                          "https://media.licdn.com/dms/image/C5603AQHc-iTXTSQ43w/profile-displayphoto-shrink_800_800/0/1613745363716?e=2147483647&v=beta&t=6BqzPbpG1fsL7HF_1a0OL9LrbWwC3cf-Y4YEtXNYDNc"),
-                    ),
-                    decoration: BoxDecoration(color: Colors.white),
-                    accountName: Text(
-                      "Shamol Chandra Das",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    accountEmail: Text(
-                      "shamolchandradas@gmail.com",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  )),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Home"),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.contact_mail),
-                title: Text("Contact"),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.email),
-                title: Text("Email"),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.phone),
-                title: Text("Phone"),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.share),
-                title: Text("Share"),
-                onTap: () {},
-              ),
-            ],
-          ),
-        ),
+      darkTheme: ThemeData(primarySwatch: Colors.blue),
+      title: "Demo",
+      home: HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo App"),
+        centerTitle: true,
       ),
+      body: Text("Hello"),
     );
   }
 }
