36fc92e8dee3f1fb37938a9e9b2aa4a8296338f6
arifvtti <arif.vtti@gmail.com>
2023-02-04 11:43:16 +0600

    tab bar with fragment

---
+++ lib/Fragment/ContactFragment.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ContactFragment extends StatelessWidget {
+  const ContactFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("ContactFragment"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'Fragment/BalanceFragment.dart';
+import 'Fragment/ContactFragment.dart';
+import 'Fragment/PersonFragment.dart';
+import 'Fragment/HomeFragment.dart';
 import "package:flutter/material.dart";
 
 main() {
@@ -79,180 +83,36 @@ class HomeActivity extends StatelessWidget {
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(10))),
         padding: EdgeInsets.all(20));
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Inventory App"),
-          centerTitle: true,
-          titleSpacing: 0,
-          elevation: 0,
-          backgroundColor: Colors.blue,
-          toolbarHeight: 50,
-          actions: [
-            IconButton(
-              onPressed: () {
-                MySnackBar("Your shopping cart is empty", context);
-              },
-              icon: Icon(Icons.shopping_cart),
-              tooltip: "Check your shopping cart",
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            MySnackBar("This is floating button action", context);
-          },
-          child: Icon(Icons.add),
-          backgroundColor: Colors.blue,
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.message), label: "Contact"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
-          ],
-          currentIndex: _currentIndex,
-          onTap: (int index) {
-            if (index == 0) {
-              MySnackBar("This home bottom navbar", context);
-              _currentIndex = 0;
-            } else if (index == 1) {
-              MySnackBar("This contact bottom navbar", context);
-              _currentIndex = 1;
-            } else {
-              MySnackBar("This profile bottom navbar", context);
-              _currentIndex = 3;
-            }
-          },
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                  padding: EdgeInsets.all(0),
-                  child: UserAccountsDrawerHeader(
-                      decoration: BoxDecoration(color: Colors.white),
-                      currentAccountPicture: Image.network(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoKJPxxwPeNvISnBbZsZHe887Ws0FnrL7o0w&usqp=CAU"),
-                      accountName: Text(
-                        "Md Arif",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      accountEmail: Text(
-                        "mdarif1129@gmail.com",
-                        style: TextStyle(color: Colors.black),
-                      ))),
-              ListTile(
-                title: Text("Home"),
-                leading: Icon(Icons.home),
-                onTap: () {
-                  MySnackBar("I am home", context);
-                },
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("My App"),
+            bottom: TabBar(isScrollable: true, tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: "Home",
               ),
-              ListTile(
-                title: Text("Contact"),
-                leading: Icon(Icons.message),
-                onTap: () {
-                  MySnackBar("I am contact", context);
-                },
+              Tab(
+                icon: Icon(Icons.contact_mail),
+                text: "Contact",
               ),
-              ListTile(
-                title: Text("Email"),
-                leading: Icon(Icons.contact_mail),
-                onTap: () {
-                  MySnackBar("I am email", context);
-                },
+              Tab(
+                icon: Icon(Icons.balance),
+                text: "Balance",
               ),
-              ListTile(
-                title: Text("Profile"),
-                leading: Icon(Icons.person),
-                onTap: () {
-                  MySnackBar("I am profile", context);
-                },
+              Tab(
+                icon: Icon(Icons.person),
+                text: "Person",
               ),
-              ListTile(
-                title: Text("Share"),
-                leading: Icon(Icons.share),
-                onTap: () {
-                  MySnackBar("I am share", context);
-                },
-              )
-            ],
+            ]),
           ),
-        ),
-        endDrawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                  padding: EdgeInsets.all(0),
-                  child: UserAccountsDrawerHeader(
-                      decoration: BoxDecoration(color: Colors.white),
-                      currentAccountPicture: Image.network(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoKJPxxwPeNvISnBbZsZHe887Ws0FnrL7o0w&usqp=CAU"),
-                      accountName: Text(
-                        "Md Arif",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      accountEmail: Text(
-                        "mdarif1129@gmail.com",
-                        style: TextStyle(color: Colors.black),
-                      ))),
-              ListTile(
-                title: Text("Home"),
-                leading: Icon(Icons.home),
-                onTap: () {
-                  MySnackBar("I am home", context);
-                },
-              ),
-              ListTile(
-                title: Text("Contact"),
-                leading: Icon(Icons.message),
-                onTap: () {
-                  MySnackBar("I am contact", context);
-                },
-              ),
-              ListTile(
-                title: Text("Email"),
-                leading: Icon(Icons.contact_mail),
-                onTap: () {
-                  MySnackBar("I am email", context);
-                },
-              ),
-              ListTile(
-                title: Text("Profile"),
-                leading: Icon(Icons.person),
-                onTap: () {
-                  MySnackBar("I am profile", context);
-                },
-              ),
-              ListTile(
-                title: Text("Share"),
-                leading: Icon(Icons.share),
-                onTap: () {
-                  MySnackBar("I am share", context);
-                },
-              )
-            ],
-          ),
-        ),
-        body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, childAspectRatio: 1.2),
-          padding: EdgeInsets.all(10),
-          itemCount: myItems.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                MySnackBar(myItems[index]["title"], context);
-              },
-              child: Container(
-                width: double.infinity,
-                height: 200,
-                child: Image.network(myItems[index]["img"]!, fit: BoxFit.fill),
-                margin: EdgeInsets.all(10),
-              ),
-            );
-          },
+          body: TabBarView(children: [
+            HomeFragment(),
+            ContactFragment(),
+            BalanceFragment(),
+            PersonFragment()
+          ]),
         ));
   }
 }
